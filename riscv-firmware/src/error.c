9a27a086794cc6471a05f3fde3aa3e79727eb7d5
jiahe000 <77484740+jiahe000@users.noreply.github.com>
2023-11-18 16:03:54 -0800

    Update error.c

--- riscv-firmware/src/error.c
+++ riscv-firmware/src/error.c
@@ -5,6 +5,23 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+uint32_t SystemCall(uint64_t* param);
+uint32_t SystemCall2(uint64_t* param1, char* param2);
+
+enum SysCallOperation
+{
+  GET_TIMER_TICKS = 1,
+  GET_MODE_CONTROL_REGISTER = 2,
+  GET_CONTROLLER_REGISTER = 3,
+  GET_INTERRUPT_PENDING_REGISTER = 4,
+  SWITCH_MODE = 5,
+  SET_BACKGROUND = 6,
+  DISPLAY_TEXT = 7,
+  SET_SMALL_SPRITE = 8,
+  SET_MEDIUM_SPRITE = 9,
+  SET_LARGE_SPRITE = 10,
+};
+
 void report_error(uint32_t error_code) {
   char errorMsg[100];
 
@@ -24,9 +41,8 @@ void report_error(uint32_t error_code) {
   }
 
   // Display the error message using a system call
-  uint64_t syscall_params_error[2];
-  syscall_params_error[0] = DISPLAY_TEXT;
-  c_syscall(syscall_params_error, errorMsg);
+  uint64_t error_display_params[] = {DISPLAY_TEXT, 160};
+  SystemCall2(error_display_params, errorMsg);
 
   // Attempt handle error
   // Restart the system
