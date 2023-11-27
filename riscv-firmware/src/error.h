2519b4f007305d5504e60570453a4eccce1442ee
jiahe000 <77484740+jiahe000@users.noreply.github.com>
2023-11-26 16:27:31 -0800

    Update error.h

--- riscv-firmware/src/error.h
+++ riscv-firmware/src/error.h
@@ -11,13 +11,8 @@
 #define ERROR_INVALID_MODE_VALUE 0x01
 #define ERROR_NULL_PARAMS 0x02
 #define ERROR_UNSUPPORTED_OPERATION 0x03
+#define ERROR_INVALID_INDEX 0x04
 
-// #define ERROR_INVALID_PARAM 0x01
-// #define ERROR_HANDLER_OPERATION 0x04
-// #define ERROR_MEMORY_ALLOCATION_FAIL 0x04
 // more error codes
-
-//Report error
-//void report_error(uint32_t error_code);
 
 #endif // ERROR_H
